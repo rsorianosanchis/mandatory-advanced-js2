a5430cf0fd7ca34630b8a168d4d7d007645b0ea2
rsoriano <rsorianosanchis@gmail.com>
2019-04-03 15:22:08 +0200

    v. 03/042019 in process

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './App.css';
+//
+import {Title} from './labbComponents/title.js';
 
 class App extends Component {
   render() {
---
+++ src/labbComponents/title.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+
+export const Title = (props)=>(<h1>{props.children}</h1>);
+class Fred extends Component{
+  render(){
+    return<p>holaaa</p>
+  }
+}
